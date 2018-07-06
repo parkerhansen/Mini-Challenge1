eadb1380dc60c2cb89203a190e41ca95c004721d
parkerhansen <pqh8t2@mail.missouri.edu>
2018-07-06 14:54:05 -0700

    Edited document.cs

--- Document/document.cs
+++ Document/document.cs
@@ -2,3 +2,35 @@ using System;
 
 namespace Document
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Document");
+
+            Console.Write("Enter the name of your document: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter the content of your document: ");
+            string content = Console.ReadLine();
+
+            try
+            {
+
+                StreamWriter sw = new StreamWriter(name + "txt");
+                sw.WriteLine(content);
+                sw.Close();
+                Console.WriteLine("{0} was successfully saved. The document contains {1} characters.", name, content.Length);
+                       
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+        }
+    }
+}
