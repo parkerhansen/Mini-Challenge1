29cf4f6e1236485dc3e0e5ddf65286547d01f2b1
parkerhansen <pqh8t2@mail.missouri.edu>
2018-07-06 13:15:03 -0700

    First Commit

---
+++ Dog/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Dog
+{
+    enum Gender { Male, Female };
+
+    class Dog
+    {
+
+        public string name;
+        public string owner;
+        public int age;
+        public int sex;
+
+        public string bark(int number)
+        {
+            while (number > 0)
+            {
+                Console.Write("Woof!");
+                number--;
+            }
+        }
+
+        public Dog()
+        {
+            Console.WriteLine("Object is being created.");
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Dog puppy = new Dog();
+            puppy.name = "Orion";
+            puppy.owner = "Shawn";
+            puppy.age = 1;
+            puppy.sex = Gender.Male;
+
+            puppy.bark(3);
+            //Console.WriteLine(puppy.getTag());
+        }
+    }
+}
