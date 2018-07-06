d33936aedcec63e88d640a67085bde69bf8dca55
parkerhansen <pqh8t2@mail.missouri.edu>
2018-07-06 15:18:58 -0700

    * .gitignore: Added Document file
    * Document.sln:
    * firsttxt:
    * Program.cs:
    * Document.csproj:

---
+++ Document/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace Document
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Document");
+
+            Console.Write("Enter the name of your document: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter the content of your document: ");
+            string content = Console.ReadLine();
+
+            try
+            {
+
+                StreamWriter streamWriter = new StreamWriter(name + ".txt");
+                streamWriter.WriteLine(content);
+                streamWriter.Close();
+                Console.WriteLine("{0} was successfully saved. The document contains {1} characters.", name, content.Length);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+        }
+    }
+}
